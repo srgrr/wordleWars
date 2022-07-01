88a0bc00d2015b21a0560691a3fd5d9e1abba1b0
Sergio Rodriguez <sergio.rodriguez@streamsets.com>
2022-07-01 11:16:42 +0200

    Add smaller file with a few words to make redis testing easier

--- dev/create_schema.py
+++ dev/create_schema.py
@@ -29,7 +29,7 @@ def parse_args():
     parser.add_argument(
         '--words-file',
         type=str,
-        default='english-words/words_alpha.txt',
+        default='test_words.txt',
         help='Path to file with words'
     )
     return parser.parse_args()
